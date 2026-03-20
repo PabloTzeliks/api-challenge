c3388c0759511ee62b080e3a55d1a6ef7ba6a1a7
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 18:18:50 -0300

    feat: implement Controller for 'ItemPedido'

--- src/main/java/senai/centroweg/api_challenge/infrastructure/web/controller/ItemPedidoController.java
+++ src/main/java/senai/centroweg/api_challenge/infrastructure/web/controller/ItemPedidoController.java
@@ -3,13 +3,15 @@ package senai.centroweg.api_challenge.infrastructure.web.controller;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import senai.centroweg.api_challenge.application.dto.request.ItemPedidoRequestDto;
+import senai.centroweg.api_challenge.application.dto.request.PedidoRequestDto;
 import senai.centroweg.api_challenge.application.dto.response.ItemPedidoResponseDto;
+import senai.centroweg.api_challenge.application.dto.response.PedidoResponseDto;
 import senai.centroweg.api_challenge.application.service.ItemPedidoService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/itens")
@@ -26,5 +28,31 @@ public class ItemPedidoController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(itemPedidoService.cadastrar(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemPedidoResponseDto> atualizarItemPedido(@RequestBody @Valid ItemPedidoRequestDto request, @PathVariable UUID id) {
+
+        return ResponseEntity.ok(itemPedidoService.atualizar(request, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPedido(@PathVariable UUID id) {
+
+        itemPedidoService.remover(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemPedidoResponseDto> buscarPedido(@PathVariable UUID id) {
+
+        return ResponseEntity.ok(itemPedidoService.acharPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemPedidoResponseDto>> buscarPedidos() {
+
+        return ResponseEntity.ok(itemPedidoService.listarTodos());
     }
 }
