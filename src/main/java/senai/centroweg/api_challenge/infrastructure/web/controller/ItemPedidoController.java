5f15365333ed62c6ef3fa39409796bbd669ebcea
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-20 16:40:46 -0300

    feat: implement inital 'ItemPedido' Controller

--- src/main/java/senai/centroweg/api_challenge/infrastructure/web/controller/ItemPedidoController.java
+++ src/main/java/senai/centroweg/api_challenge/infrastructure/web/controller/ItemPedidoController.java
@@ -1,7 +1,11 @@
 package senai.centroweg.api_challenge.infrastructure.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import senai.centroweg.api_challenge.application.dto.request.ItemPedidoRequestDto;
+import senai.centroweg.api_challenge.application.dto.response.ItemPedidoResponseDto;
 import senai.centroweg.api_challenge.application.service.ItemPedidoService;
 
 @RestController
@@ -14,5 +18,9 @@ public class ItemPedidoController {
         this.itemPedidoService = itemPedidoService;
     }
 
+    public ResponseEntity<ItemPedidoResponseDto> cadastrarItemPedido(ItemPedidoRequestDto request) {
 
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(itemPedidoService.cadastrar(request));
+    }
 }
